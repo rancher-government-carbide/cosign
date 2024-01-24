f37280117656c29953d92e6e388eb2164b880cb1
Adam Martin <adam.martin@rancherfederal.com>
2024-01-24 16:07:23 -0500

    add platform flag to cosign save

--- cmd/cosign/cli/save.go
+++ cmd/cosign/cli/save.go
@@ -25,6 +25,7 @@ import (
 	"github.com/sigstore/cosign/v2/pkg/oci"
 	"github.com/sigstore/cosign/v2/pkg/oci/layout"
 	ociremote "github.com/sigstore/cosign/v2/pkg/oci/remote"
+	ociplatform "github.com/sigstore/cosign/v2/pkg/oci/platform"
 	"github.com/spf13/cobra"
 )
 
@@ -39,7 +40,7 @@ func Save() *cobra.Command {
 		Args:             cobra.ExactArgs(1),
 		PersistentPreRun: options.BindViper,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return SaveCmd(cmd.Context(), *o, args[0])
+			return SaveCmd(cmd.Context(), *o, args[0], o.Platform)
 		},
 	}
 
@@ -47,7 +48,7 @@ func Save() *cobra.Command {
 	return cmd
 }
 
-func SaveCmd(_ context.Context, opts options.SaveOptions, imageRef string) error {
+func SaveCmd(_ context.Context, opts options.SaveOptions, imageRef string, platform string) error {
 	ref, err := name.ParseReference(imageRef)
 	if err != nil {
 		return fmt.Errorf("parsing image name %s: %w", imageRef, err)
@@ -56,6 +57,11 @@ func SaveCmd(_ context.Context, opts options.SaveOptions, imageRef string) error
 	se, err := ociremote.SignedEntity(ref)
 	if err != nil {
 		return fmt.Errorf("signed entity: %w", err)
+	}
+
+	se, err = ociplatform.SignedEntityForPlatform(se, platform)
+	if err != nil {
+		return err
 	}
 
 	if _, ok := se.(oci.SignedImage); ok {
